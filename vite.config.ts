4d7dac96878a2128a7c607fb36e13397fc13148e
l4mbh <lambh.1998@gmail.com>
2025-12-27 00:15:19 +0700

    ci: setup GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_PAGES === 'true' ? '/autopot2-release-public/' : '/',
+  build: {
+    outDir: 'dist',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
